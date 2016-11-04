4356c7cab7e896eac349575f4907bde174529aad
katebeavis <katebeavis84@gmail.com>
2016-11-04 10:53:47 +0000

    fix issue with pr state

--- app/controllers/github_webhooks_controller.rb
+++ app/controllers/github_webhooks_controller.rb
@@ -5,11 +5,11 @@ class GithubWebhooksController < ApplicationController
   def payload
     @client = HipchatApi.new
     @event = params
+    binding.pry
     state = @event[:github_webhook][:action]
     user = @event[:pull_request][:user][:login]
     link = @event[:pull_request][:html_url]
-    if state = 'opened'
-      binding.pry
+    if state == 'opened' || state == 'reopened'
       @client.send_message("Notifications", "Pull request #{state} by #{user} <a href=#{link}>#{link}</a>")
       @client.send_message("Notifications", "@KateBeavis should take a look", :message_format => "text")
     end
